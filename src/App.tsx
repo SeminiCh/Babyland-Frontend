936c27c1004c120c8755680c6bccc7ae76c5bc48
SeminiCh <seminipathirana@gmail.com>
2022-07-12 20:06:56 +0530

    Nanny Card Created

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Navigationbarcustomer from "./Lib/Navigationbarcustomer";
 import HomeCustomer from "./Lib/HomeCustomer";
+import NannyCard from "./Components/NannyCard";
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
     <>
       <Navigationbarcustomer />
       <HomeCustomer />
+      <br />
+      <br />
+      <NannyCard />
     </>
   );
 }
